941aeff50bda9e6bbec42140d9460984ac401e8c
hugonun <43740923+hugonun@users.noreply.github.com>
2019-04-11 18:06:02 +0200

    Add files via upload

---
+++ gsheet.py
@@ -0,0 +1,42 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+class gsheet(object):
+    def __init__(self):
+        SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+        self.creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                self.creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not self.creds or not self.creds.valid:
+            if self.creds and self.creds.expired and self.creds.refresh_token:
+                self.creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                self.creds = flow.run_local_server()
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(self.creds, token)
+
+        self.service = build('sheets', 'v4', credentials=self.creds)
+    def add(self,sheetid,sheetrange,ivalue):
+        # Call the Sheets API
+        sheet = self.service.spreadsheets()
+        values = []
+        values.append(ivalue)
+        body = {
+            'values': values
+        }
+        result = sheet.values().append(
+            spreadsheetId=sheetid, range=sheetrange,
+            valueInputOption='RAW', body=body).execute()
+        
---
+++ init.py
@@ -0,0 +1,45 @@
+import discord
+
+from gsheet import *
+
+client = discord.Client()
+sheet = gsheet()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    # Command to insert data to excel
+    if message.content.startswith('!s '):
+        SPREADSHEET_ID = '2QkhiqSFomDLYYYQUt3LO_IuUu72qEtXsLDc4Blcf5dq' # Add ID here
+        RANGE_NAME = 'A1'
+        FIELDS = 2 # Amount of fields/cells
+
+        # Code
+        msg = message.content[3:]
+        result = [x.strip() for x in msg.split(',')]
+        if len(result) == FIELDS:
+            # Add
+            print(message.created_at)
+            DATA = [message.author.name] + [message.author.id] + [str(message.created_at)] + result
+            sheet.add(SPREADSHEET_ID, RANGE_NAME, DATA)
+            await message.channel.send('Your data has been successfully submitted!')
+        else:
+            # Needs more/less fields
+            await message.channel.send('Error: You need to add {0} fields, meaning it can only have {1} comma.'.format(FIELDS,FIELDS-1))
+    
+    # Whois
+    # Please dont remove the copyright and github repo
+    elif len(message.mentions) > 0:
+        for muser in message.mentions:
+            if muser.id == client.user.id:
+                if any(word in message.content for word in ['whois','who is','Help','help','info']):
+                    await message.channel.send('This bot was made by hugonun(https://github.com/hugonun/).\nSource code: https://github.com/discord2excel-bot')
+
+
+client.run('') # Add bot token here
